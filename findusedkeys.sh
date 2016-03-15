279d40b423a699910d0253aac083af5ece92a8e3
Tama McGlinn <tama@innovattic.com>
2016-03-15 17:04:13 +0100

    Added a script to find keys that are actually used in a project, and print out the calls that you will need to examine yourself (first used for CVIS)

---
+++ findusedkeys.sh
@@ -0,0 +1,25 @@
+# Use this from the root of a Unity project to find all UILocalize components in scenes, prefabs, and C# code
+# It overwrites your usedkeys.txt with a new version that contains all the simple keys; 
+# if you have code that constructs keys without directly passing them to Localization.Get, this script will not find them correctly
+# but will output the C# code and filename that used Localization.Get in the non-standard manner
+
+# find in scenes
+grep "key: " Assets/Scenes/*.unity | cut -d' ' -f4 > usedkeys.txt
+# find in prefabs
+find Assets/Prefabs/ -type f -name "*.prefab" | xargs -I {} grep "key: " "{}" | cut -d' ' -f4 >> usedkeys.txt
+# find in C# code; Localization.Get("key")
+find Assets/Scripts -name *.cs | xargs grep -oh "Localization.Get(\"[^\)]*\")" | cut -d'"' -f2 >> usedkeys.txt
+# find stringReferences in properties file
+grep ".stringReference" Assets/objects.properties.txt | cut -d'=' -f2 | tr -d ' ' | sort | uniq >> usedkeys.txt
+grep ".type =" Assets/objects.properties.txt | tr -d ' ' | grep -v "route" | grep -v "island" | cut -d'.' -f1 | sort | uniq >> usedkeys.txt
+
+
+sort usedkeys.txt | uniq > tmp.txt
+mv tmp.txt usedkeys.txt
+
+#print the rest
+echo "The rest are non-standard usages of Localization.Get(), with a variable passed in, or other bullshit you will have to find out for yourself:"
+
+find Assets/Scripts -name *.cs | xargs grep "Localization.Get" | grep -v "Localization.Get(\"[^\)]*\")"
+
+echo "Afterwards, you will want to do something like; `sort usedkeys.txt | uniq > tmp.txt; mv tmp.txt usedkeys.txt` to get rid of any duplicates you may have introduced"
